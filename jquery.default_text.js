3eafdcc0a3f4db6cefc0d4eff29d2f22469be955
tanin <tanin47@yahoo.com>
2011-06-30 23:53:35 +0700

    add readme and fix stuff

--- jquery.default_text.js
+++ jquery.default_text.js
@@ -1,48 +1,65 @@
 /*
  *  Do whatever you want with it. I don't care
- *  But don't fotget to give me some credit :P
- *  - Tanin Na Nakorn (whowish.com)
+ *  But don't fotget to give us some credit. (Apache License)
+ *  - Tanin Na Nakorn
  *  - Sergiu Rata
  */
 (function($){
     $.fn.extend({         
         //pass the options variable to the function
         default_text: function(msg, opts) {
+			
+			if (opts == undefined) { opts = {}; }
+			
             var defaultTextClassName = opts.defaultClass != null ? opts.defaultClass : "jquery_default_text";
             var defaultIdSuffix = opts.defaultIdSuffix != null ? opts.defaultIdSuffix : "_default_text";
 
             return this.each(function() {
                 var real_obj = this;
-                var default_obj = $(this).clone();
-                $(default_obj).hide();
+				var default_obj = null;
+				
+				if ($('#' + $(this)[0].id + defaultIdSuffix).length == 0) {
+					default_obj = $(this).clone();
+					$(default_obj).css('display',$(real_obj).css('display'));
+					$(default_obj).attr('name',$(real_obj).attr('name')+defaultIdSuffix);
+					$(default_obj).hide();
+					
+					$(default_obj).insertBefore(this);
+					$(default_obj).val(msg).addClass(defaultTextClassName).attr('id', $(this)[0].id + defaultIdSuffix);
+					
+					if ($(real_obj).val() == "") {
+	                    $(real_obj).hide();
+	                    $(default_obj).show();
+	                } else {
+	                    $(real_obj).show();
+	                    $(default_obj).hide();
+	                }
+	                
+	                var defaultClickHandler = function() {
+						if ($(real_obj).css('display') == 'none') {
+							$(default_obj).hide();
+							$(real_obj).show();
+							$(real_obj).focus();
+						}
+	                };
+	
+	                $(default_obj).click(defaultClickHandler);
+	                $(default_obj).mousedown(defaultClickHandler);
+	                $(default_obj).focus(defaultClickHandler);
+	                
+	                $(real_obj).blur(function() {
+	                    if ($(real_obj).val() == "") {
+	                        $(default_obj).show();
+	                        $(real_obj).hide();
+	                    }
+	                });
+					
+				} else
+				{
+					$('#' + $(this)[0].id + defaultIdSuffix).val(msg);
+				}
                 
-                $(default_obj).insertBefore(this);
-                $(default_obj).val(msg).addClass(defaultTextClassName).attr('id',$(this)[0].id + defaultIdSuffix);
                 
-                if ($(real_obj).val() == "") {
-                    $(real_obj).hide();
-                    $(default_obj).show();
-                } else {
-                    $(real_obj).show();
-                    $(default_obj).hide();
-                }
-                
-                var defaultClickHandler = function() {
-                    $(default_obj).hide();
-                    $(real_obj).show();
-                    $(real_obj).focus();
-                };
-
-                $(default_obj).click(defaultClickHandler);
-                $(default_obj).mousedown(defaultClickHandler);
-                $(default_obj).focus(defaultClickHandler);
-                
-                $(real_obj).blur(function() {
-                    if ($(real_obj).val() == "") {
-                        $(default_obj).show();
-                        $(real_obj).hide();
-                    }
-                });
             });
         }        
     });    
